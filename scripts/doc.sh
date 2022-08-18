04c669a168bc32509192e22516581bd7bd167b1c
Lanna Michalke <l.michalke@famedly.com>
2022-08-18 10:36:09 +0200

    fix: CI hotfix
    Signed-off-by: Lanna Michalke <l.michalke@famedly.com>

--- scripts/doc.sh
+++ scripts/doc.sh
@@ -3,5 +3,5 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
 
 cd "$(dirname "$0")"/..
-dart doc --exclude dart:async,dart:collection,dart:convert,dart:core,dart:developer,dart:io,dart:isolate,dart:math,dart:typed_data,dart:ui
+dart doc
 sed -i '/https:\/\/famedly.gitlab.io\/libraries\/dart-olm\/#architecture/!b;g;r doc/architecture.inc' doc/api/index.html
